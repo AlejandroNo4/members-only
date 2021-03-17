0779d4e10aed2dfd64339e7e0ed83d3ba2eb35e6
Alejandro Contreras <kdta15@outlook.com>
2021-03-17 17:50:10 -0600

    Run and fixed rubocop offenses

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,14 +1,9 @@
 module HomeHelper
-
   def signed_user(post)
-    if user_signed_in?
-      "By: #{post.user.email}"
-    end
+    "By: #{post.user.email}" if user_signed_in?
   end
 
   def create_post_link
-    if user_signed_in?
-      link_to "Create a Post", new_post_path, class: "btn btn-primary"
-    end
+    link_to 'Create a Post', new_post_path, class: 'btn btn-primary' if user_signed_in?
   end
 end
