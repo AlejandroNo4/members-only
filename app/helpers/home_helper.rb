20c7a0c88e7e3336d0798880527b9c1e02d61c91
Alejandro Contreras <kdta15@outlook.com>
2021-03-17 17:41:30 -0600

    Move the logic from the _navbar and index views to their respecrive helpers

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,14 @@
 module HomeHelper
+
+  def signed_user(post)
+    if user_signed_in?
+        post.user.email
+    end
+  end
+
+  def create_post_link
+    if user_signed_in?
+      link_to "Create a Post", new_post_path, class: "btn btn-primary"
+    end
+  end
 end
